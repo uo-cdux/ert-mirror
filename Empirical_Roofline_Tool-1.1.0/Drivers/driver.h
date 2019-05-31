d9edf90f123b51d1e9c6c71e364d26e80d284590
Yunsong Wang <yunsongwang@lbl.gov>
2019-05-30 17:29:18 -0700

    Clean up

--- Empirical_Roofline_Tool-1.1.0/Drivers/driver.h
+++ Empirical_Roofline_Tool-1.1.0/Drivers/driver.h
@@ -7,8 +7,6 @@
 #include <sys/time.h>
 #include <inttypes.h>
 
-#include "Array.h"
-
 #ifdef ERT_OPENMP
   #include <omp.h>
 #endif
@@ -30,33 +28,5 @@
 #endif
 
 #define GBUNIT (1024 * 1024 * 1024)
-
-template <typename T>
-class Driver {
-private:
-  int fMPIRank;
-  int fMPISize;
-  int fNThreads;
-
-#ifdef ERT_GPU
-  int fGPUBlocks;
-  int fGPUThreads;
-#endif
-
-#ifdef ERT_MPI
-  int fMPIProvided;
-  #ifdef ERT_OPENMP
-  int fMPIRequested;
-  #endif
-#endif
-
-  Array<T> fArray;
-
-public:
-  Driver();
-  ~Driver();
-
-  void Run();
-};
 
 #endif
